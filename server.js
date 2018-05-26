465c5f31c816be0ffe114dd0a7a0353793d489c1
panjamapon karnasuta <happyudongstudio@gmail.com>
2018-05-26 13:59:17 +0700

    learn nodeJS course

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static(__dirname + "/views"));
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("home", {
+    user: {
+      name: 'happyudong'
+    }
+  });
+});
+
+// web server
+app.listen(3000, () => {
+  console.log("server is running at localhost:3000");
+});
